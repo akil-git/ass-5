e0e88ac3a55efced935487f1812aff8cabddf7db
akil <sayemakil456@gmail.com>
2024-02-19 23:45:25 +0600

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const seats = document.getElementsByClassName('seat')
 let selectedSeat = 0 
-let price = 550 
+let prices = 550 
 console.log(seats);
 for (const seat of seats) {
     seat.addEventListener('click',function(event){
@@ -14,6 +14,40 @@ for (const seat of seats) {
     const elementsId = event.target.id;
     console.log(elementsId);
     setBackgroundById(elementsId);
+
+// total ticket 
+    const selectSeatElement = document.getElementById('ticket-count')
+    const selectedSeats = selectSeatElement.innerText
+    const addSeat = parseInt(selectedSeats)
+    const seatss = addSeat + 1 
+    selectSeatElement.innerText = seatss
+
+    // total calculation
+const priceElement = document.getElementById('total')
+const priceText = priceElement.innerText
+const priceNumber = parseInt(priceText)
+const x = parseInt(prices)
+const pricess = priceNumber + x
+priceElement.innerText = pricess
+
+// grand total 
+const grandPriceElement = document.getElementById('grand')
+const grandPriceText = grandPriceElement.innerText
+const grandPriceNumber = parseInt( grandPriceText)
+const xx = parseInt(prices)
+const pricesss = grandPriceNumber + xx
+grandPriceElement.innerText = pricesss
+
+    // left seat
+    const leftSeats = document.getElementById('seat-number')
+    const seatText = leftSeats.innerText
+    const leftSeat = parseInt(seatText)
+    const seatLeft = leftSeat -1 
+    leftSeats.innerText = seatLeft
+
+   
+ 
+
     })
 }
 document.getElementById('Continue').addEventListener('click',function () {
@@ -24,3 +58,4 @@ function setBackgroundById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-green-500')
 }
+
